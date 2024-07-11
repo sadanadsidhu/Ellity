a191479619de244d66f36fa54a76375fb8b01775
Sadanand Kumar Yadav <sadanandsidhu@gmail.com>
2024-07-11 15:45:04 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
 import DeskTop from "./desktop/DeskTop";
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Routes>
-        {/* {" "} */}
-        {/* Wrap your routes with <Routes> element */}
-        {/* <Route path="/" exact element={<PersonalDataForm />} />
-        <Route path="/contact-details" element={<ContactDetails />} /> */}
-        <Route path="/" element={<DeskTop/>} />
-        {/* Add more routes for other components if needed */}
-      </Routes>
-    </div>
-  </Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<DeskTop />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
